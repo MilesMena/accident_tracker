253387dab04d6d12ee54ef12b647a02f374524e3
MilesMena <menamil28@outlook.com>
2024-01-30 12:50:53 -0700

    add date_counts and sunburts
    fix #4

--- plotting.py
+++ plotting.py
@@ -63,3 +63,40 @@ def accidents_percent(df, perc_var, drop_na = True):
     fig.show()
 
     return melted_df
+
+def plot_date_counts(df, var):
+    date_counts = df[df[var] != 'NO DATA'].groupby(['Crash Date', var]).size().unstack(fill_value=0).reset_index()
+    
+    #county_perc = (county_counts.div(county_counts.sum(axis=1), axis=0) * 100).reset_index()
+    #melted_df = pd.melt(county_perc, id_vars=['County'], var_name=perc_var, value_name='Percentage')
+    not_date = [col for col in date_counts if col != 'Crash Date']
+    fig = px.line(date_counts, x = 'Crash Date', y = not_date)
+    
+    dropdowns = []
+    for column in not_date:
+        if column != 'Crash Date':
+            dropdown = dict(label=column, method='update', args=[{'y': [date_counts[column]]}])
+            dropdowns.append(dropdown)
+                
+    fig.update_layout(title=f'Accident Counts on Date (by {var})',xaxis=dict(title='Date'),yaxis=dict(title='Animal Counts'),height=600,width=1200,
+                #updatemenus= [dict(type = 'dropdown', x = -.02, y = 1, buttons= dropdowns)]
+    ) 
+    
+    fig.show()
+
+    return date_counts
+
+def get_sunburst(df, path_cols):
+    
+    df_filtered = df[~df[path_cols].apply(lambda row: any(value == 'NO DATA' for value in row), axis=1)]
+
+    
+    data = df_filtered.groupby(path_cols)['Ones'].sum().reset_index()
+    fig = px.sunburst(
+        data,
+        path=path_cols, values='Ones'
+    )
+    
+    fig.update_layout(height = 400, width = 400)
+    
+    fig.show()
